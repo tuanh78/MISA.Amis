ed96d8399979197592564df17a0429f9978ce38c
tuanh78 <phamtuanh2000@gmail.com>
2021-06-18 16:59:08 +0700

    xu ly exception

--- MISA.AMIS.API/Startup.cs
+++ MISA.AMIS.API/Startup.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -9,6 +11,7 @@ using MISA.AMIS.Core.Interfaces.Repositories;
 using MISA.AMIS.Core.Interfaces.Services;
 using MISA.AMIS.Core.Services;
 using MISA.AMIS.Infrastructure.Repositories;
+using System;
 
 namespace MISA.AMIS.API
 {
@@ -28,7 +31,24 @@ namespace MISA.AMIS.API
             services.AddControllers().AddNewtonsoftJson();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MISA.AMIS.API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "MISA Amis API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Pham Tu Anh",
+                        Email = string.Empty,
+                        Url = new Uri("https://www.facebook.com/profile.php?id=100010235404139"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
             });
 
             services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
@@ -49,6 +69,16 @@ namespace MISA.AMIS.API
             }
 
             app.UseHttpsRedirection();
+
+            // handle exception
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features
+                    .Get<IExceptionHandlerPathFeature>()
+                    .Error;
+                var response = new { error = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
 
             app.UseRouting();
 
